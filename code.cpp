c435e667cf2cb03ad06e8714b92b7e5bf65e2886
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-01-14 22:31:57 +0530

    i made this template of my competitive programming code

---
+++ code.cpp
@@ -0,0 +1,18 @@
+/* This project template is created by LordOfWizard
+ * https://github.com/lordofwizard
+ * YouTube https://0x0.st/NUMD
+ */
+
+#include <bits/stdc++.h>
+#include <vector>
+#include <math>
+
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		
+	}
+}
